c93ad9fea846056e81a8b0b1026c6e59d0991c03
Kristina Danica Bustillos <kristina.bustillos@recruitday.com>
2023-08-11 22:24:26 +0800

    fixed broken imgs

--- js/general-scripts.js
+++ js/general-scripts.js
@@ -1,21 +1,4 @@
 function generalScripts() {
-	// ---------general imgs--------
-    if($('main').hasClass('insidepage')) {
-        $(".nav-logo img").attr('src', '../images/icons/OL-logo.svg');
-        $(".footer-logo img").attr('src', '../images/icons/OL-logo.svg');
-        $("img.fb-ico").attr('src', '../images/icons/fb.svg');
-        $("img.tw-ico").attr('src', '../images/icons/tw.svg');
-        $("img.in-ico").attr('src', '../images/icons/in.svg');
-        $("img.yt-ico").attr('src', '../images/icons/yt.svg');
-    } else {
-        $(".nav-logo img").attr('src', 'images/icons/OL-logo.svg');
-        $(".footer-logo img").attr('src', 'images/icons/OL-logo.svg');
-        $("img.fb-ico").attr('src', 'images/icons/fb.svg');
-        $("img.tw-ico").attr('src', 'images/icons/tw.svg');
-        $("img.in-ico").attr('src', 'images/icons/in.svg');
-        $("img.yt-ico").attr('src', 'images/icons/yt.svg');
-    }
-
 	// -------navigation------------------
     $("#nav-icon3").click(function(){
 		$(".nav-links-content").animate({
